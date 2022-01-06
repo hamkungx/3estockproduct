6eead3df9d15b969b2cdec86e70601ba44a85b3b
Ronnagid Kongsuk <83935843+hamkungx@users.noreply.github.com>
2022-01-06 10:37:46 +0700

    ื

--- myproject/stockproduct/urls.py
+++ myproject/stockproduct/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns=[
     path('',index,name='index'),  
+    path('createproduct',createproduct,name='createproduct'),  
+    
 ]
--- myproject/stockproduct/views.py
+++ myproject/stockproduct/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,"frontend/index.html")
+def createproduct(request):
+    return render(request,"frontend/createproduct.html")
